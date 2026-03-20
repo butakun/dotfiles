18bfa41adb220188355fa5d1462a0d703c906c7c
Hiromasa Kato <hka@trackman.com>
2026-03-20 11:11:19 +0100

    editline

--- pythonrc.py
+++ pythonrc.py
@@ -1,4 +1,10 @@
 import readline
 import rlcompleter
-readline.parse_and_bind("set editing-mode vi")
-readline.parse_and_bind("tab: complete")
+
+if "libedit" in readline.__doc__:
+    readline.parse_and_bind("bind -v")
+    readline.parse_and_bind("bind ^I rl_complete")
+    #readline.parse_and_bind('bind "kj" vi-command-mode')
+else:
+    readline.parse_and_bind("set editing-mode vi")
+    readline.parse_and_bind("tab: complete")
